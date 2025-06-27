ead092d4cfec94eacdc01bf6b56e6f74a2906970
Etele Kovács <etikova777@gmail.com>
2025-06-27 16:10:18 +0200

    set up 3 channel yolo training

---
+++ predict.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Load your custom model
+model = YOLO("Yolo/runs/detect/train/weights/bestv8_better.pt")  # Replace with your actual model path
+
+
+# Run prediction on an image
+results = model("Yolo/data/test_for_prediction", save=True)
